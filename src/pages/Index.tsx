528140df5cb02dfe9a6e858cdc67a9e4823aa794
Adam <am2826@kent.ac.uk>
2025-11-20 23:19:50 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,8 +9,8 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <Hero />
-      <About />
       <Menu />
+      <About />
       <Reviews />
       <Location />
       <Footer />
